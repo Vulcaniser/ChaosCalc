117e341a0c960f3b3fa0dc1d1e2fc3bcd0ac6a32
Vulcaniser <parashar.nkp28@gmail.com>
2026-07-12 21:54:41 +0530

    Defined the first set of functions

---
+++ script.js
@@ -0,0 +1,13 @@
+const sum=(a,b)=> a+b
+const prod=(a,b)=> a*b
+const sub=(a,b)=> a-b
+const divide=(a,b)=> a/b
+const expo=(a,b)=> a**b
+const sine=(a)=>Math.sin(a)
+const cosine=(a)=>Math.cos(a)
+const tangent=(a)=>Math.tan(a)
+const cotangent=(a)=>1/Math.tan(a)
+const cosec=(a)=>1/Math.sin(a)
+const sec=(a)=>1/Math.cos(a)
+
+
